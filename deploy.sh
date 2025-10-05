5b81ca00fa7808731508dbbea856c6be8b645a41
saians07 <suberlinsinaga@gmail.com>
2025-10-05 17:08:23 +0700

    Improve deploy.sh to enhance the use of green and blue deployment

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-echo "Start deploying to server"
+echo "Start deploying process ..."
 
 source .env
 
-cd ~/Desktop/projects/triple-s
-cp ~/Desktop/very_private_repo/.env .env
-# docker pull ghcr.io/saians07/triple_s:latest
+cd ~/backend_pi_5
+cp ~/very_private_repo/.env .env
+docker pull ghcr.io/saians07/backend_pi_5:latest
 
-# Determine current and next environment
-if docker ps --format "table {{.Names}}" | grep -q "triple_s_green"; then
+echo "Start configuring environment ..."
+if docker ps --format "table {{.Names}}" | grep -q "backend_pi_5_blue"; then
     CURRENT="blue"
     NEXT="green"
     CURRENT_PORT=8082
@@ -19,19 +19,41 @@ else
     NEXT_PORT=8082
 fi
 
-echo "$NEXT"
+echo "Deploying to: $NEXT environment (port: $NEXT)"
+docker compose --file docker_compose_$NEXT.yml down
+docker compose --file docker_compose_$NEXT.yml up --build -d
 
-apt install jq
+sleep 10
 
-TOKEN=$(curl -X POST http://nginx.triple-s.web.id/api/users/login -H "Content-Type: application/json" -d "{\"username\":\"$NGINX_IGNITION_USER\",\"password\":\"$NGINX_IGNITION_PWD\"}" | jq -r '.token')
+echo "🩺 Start health checking ..."
+if curl -f -s --max-time 30 http://localhost:$NEXT_PORT/health; then
+    echo "✅ Health check Passed"
 
-RAW_DATA="{\"enabled\":true,\"defaultServer\":false,\
-    \"useGlobalBindings\":false,\"domainNames\":[\"api.triple-s.web.id\"],\
-    \"routes\":[{\"priority\":0,\"enabled\":true,\"type\":\"PROXY\",\"sourcePath\":\"/\",\"settings\":{\"includeForwardHeaders\":true,\"proxySslServerName\":true,\"keepOriginalDomainName\":true,\"directoryListingEnabled\":false,\"custom\":null},\"targetUri\":\"http://192.168.1.10:$NEXT_PORT\",\"redirectCode\":null,\"response\":null,\"integration\":null,\"accessListId\":null,\"sourceCode\":null}],\
-    \"bindings\":[{\"type\":\"HTTP\",\"ip\":\"0.0.0.0\",\"port\":80,\"certificateId\":null}],\
-    \"featureSet\":{\"websocketsSupport\":true,\"http2Support\":true,\"redirectHttpToHttps\":false},\
-    \"accessListId\":null}"
+     if docker ps --format "table {{.Names}}" | grep -q "backend_pi_5_$CURRENT"; then
+        echo "Removing $CURRENT environment"
+        docker compose --file docker_compose_$CURRENT.yml down
+    fi
 
-curl -X PUT http://nginx.triple-s.web.id/api/hosts/ed2557da-24bf-4fa1-8f8a-4a0761a1d8b5 \
-    -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" \
-    --data-raw "$RAW_DATA"
+    # install jq to parse json
+    apt install jq
+
+    # get the token from nginx json response
+    TOKEN=$(curl -X POST http://nginx.$BACKEND_URL/api/users/login -H "Content-Type: application/json" -d "{\"username\":\"$NGINX_IGNITION_USER\",\"password\":\"$NGINX_IGNITION_PWD\"}" | jq -r '.token')
+
+    RAW_DATA="{\"enabled\":true,\"defaultServer\":false,\
+        \"useGlobalBindings\":false,\"domainNames\":[\"api.$BACKEND_URL\"],\
+        \"routes\":[{\"priority\":0,\"enabled\":true,\"type\":\"PROXY\",\"sourcePath\":\"/\",\"settings\":{\"includeForwardHeaders\":true,\"proxySslServerName\":true,\"keepOriginalDomainName\":true,\"directoryListingEnabled\":false,\"custom\":null},\"targetUri\":\"http://192.168.1.10:$NEXT_PORT\",\"redirectCode\":null,\"response\":null,\"integration\":null,\"accessListId\":null,\"sourceCode\":null}],\
+        \"bindings\":[{\"type\":\"HTTP\",\"ip\":\"0.0.0.0\",\"port\":80,\"certificateId\":null}],\
+        \"featureSet\":{\"websocketsSupport\":true,\"http2Support\":true,\"redirectHttpToHttps\":false},\
+        \"accessListId\":null}"
+
+    curl -X PUT http://nginx.$BACKEND_URL/api/hosts/ed2557da-24bf-4fa1-8f8a-4a0761a1d8b5 \
+        -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" \
+        --data-raw "$RAW_DATA"
+
+    echo "✅ Deployment successful to $NEXT environment"
+else
+    echo "❌ Health check failed on $NEXT environment"
+    docker compose --file docker_compose_$NEXT.yml down
+    exit 1
+fi
