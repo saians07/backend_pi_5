4e46590d7690112bf3c0df686ae4b4892e5ae8b6
Suberlin Sinaga <suberlinsinaga@gmail.com>
2025-10-10 00:34:56 +0700

    Feature move from flask to fastapi (#15)
    * Change API from flask to fastapi since we only need backend.
    
    * Change requirements from Flask to fastapi
    
    * Since we move the framework, we move the version to be 0.2.0 in the beta state.
    
    * Rename step on CI to be more representative
    
    * Include docstring and remove unused code for linting purpose

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 # pylint: disable=C0114
-from flask import Flask, jsonify, request # pylint: disable=C0114
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI(title="Backend Raspberry Pi")
 
-@app.route("/health", methods=["GET", "POST"])
+@app.get("/")
+def root():
+    """Visit root path"""
+    return {'status': "error"}
+
+@app.get("/health")
 def health(): # pylint: disable=C0116
-    return jsonify({
-        'status': "ok"
-    }), 200
-
-BASE_API = "/api/v1/"
-
-@app.route(f"{BASE_API}create_table", methods=["POST"])
-def create_table(): # pylint: disable=C0116
-    data = request.json
-
-    if not data.get("table_name"):
-        return jsonify(
-            {
-                'status': "FAILED",
-                'message': "You have to pass table_name when creating a table"
-            }
-        ), 400
-
-    return jsonify({
-        'status': "OK"
-    }), 200
+    """Check health for the page"""
+    return {'status': "ok", 'status_code': 200}
