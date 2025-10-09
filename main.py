a36535f8a90c339f998b6a4841290bb5653044c9
saians07 <suberlinsinaga@gmail.com>
2025-10-10 00:31:47 +0700

    Include docstring and remove unused code for linting purpose

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # pylint: disable=C0114
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 
 app = FastAPI(title="Backend Raspberry Pi")
 
 @app.get("/")
 def root():
+    """Visit root path"""
     return {'status': "error"}
 
 @app.get("/health")
 def health(): # pylint: disable=C0116
+    """Check health for the page"""
     return {'status': "ok", 'status_code': 200}
