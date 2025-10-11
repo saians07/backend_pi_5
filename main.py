cf8fb820f01ad436e3a37c894542a01b49c21538
saians07 <suberlinsinaga@gmail.com>
2025-10-12 00:01:55 +0700

    Use lifcycle to ensure client httpx only expand once for the lifetime of the worker.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 # pylint: disable=C0114
 from fastapi import FastAPI, HTTPException, status
+import httpx
 from api import telegram_router
 from core.logger import LOG
+from core.telegram import TelegramBot
 
 LOG.info("Starting Backend Pi 5 Applications ...")
 
-app = FastAPI(title="Backend Raspberry Pi")
+async def lifespan(app: FastAPI):
+    client = httpx.AsyncClient()
+    bot = TelegramBot(client)
+    app.state.bot = bot
+    yield
+    await client.aclose()
+
+app = FastAPI(title="Backend Raspberry Pi", lifespan=lifespan)
 
 @app.get("/", status_code=status.HTTP_403_FORBIDDEN)
 def root():
