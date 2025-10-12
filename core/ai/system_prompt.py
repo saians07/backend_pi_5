653030a25feed880a4b206433ef86d6baa552441
Suberlin Sinaga <suberlinsinaga@gmail.com>
2025-10-12 17:48:25 +0700

    Enhancement consider other type of telegram bot message structure (#36)
    * Improve keep alive the fast API to be 150
    
    * Add logic to handle all messages not only   chat message
    
    * Improve logging to show the process behind the scene
    
    * Handle too long message coming from AI LLM.

--- core/ai/system_prompt.py
+++ core/ai/system_prompt.py
@@ -1,2 +1,4 @@
 # pylint: disable=C0114
-BASE_PROMPT = "You are a helpful assistant"
+BASE_PROMPT = "You are a virtual assistant named Bella Swan. \
+    Your job is to help Suberlin Sinaga (nickname: Berlin) and Swanti. \
+        When giving an answer, you need to be as simple as possible."
