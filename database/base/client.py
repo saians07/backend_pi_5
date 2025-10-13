56cde75e27478e4eb64779407837731d47a19ca1
saians07 <suberlinsinaga@gmail.com>
2025-10-13 23:02:28 +0700

    Add database configuration to handle various database needs.

---
+++ database/base/client.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+load_dotenv()
+
+DATABASE_HOST = os.getenv("PI5_POSTGRES_DB_HOST", "localhost")
+DATABASE_USER = os.getenv("PI5_POSTGRES_DB_USER", "postgres")
+DATABASE_PWD = os.getenv("PI5_POSTGRES_DB_PWD", "password")
+DATABASE_PORT = os.getenv("PI5_POSTGRES_DB_PORT", "5432")
+DATABASE_NAME = os.getenv("PI5_POSTGRES_DB_NAME", "postgres")
+DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PWD}@\
+{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+
+engine = create_engine(
+    DATABASE_URL
+)
+
+DBSession = sessionmaker(autoflush=False, autocommit=False, bind=engine)
---
+++ database/telegram_bot_chat/schema.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from typing import (
+    Optional
+)
+from sqlalchemy import (
+    String,
+    BigInteger,
+    Boolean,
+    DateTime,
+    func
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column
+)
+from database.base import Base
+
+class BotChatHistory(Base):
+    __tablename__ = 'telegram_bot_chat_history'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    update_id: Mapped[int] = mapped_column(BigInteger)
+    parent_id: Mapped[Optional[int]] = mapped_column(BigInteger, nullable=True)
+    user_id: Mapped[int] = mapped_column(BigInteger)
+    created_datetime: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    updated_datetime: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
+    deleted_datetime: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
+
+class BotUserMapping(Base):
+    __tablename__ = 'telegram_bot_user_mapping'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(BigInteger)
+    user_name: Mapped[str] = mapped_column(String(100))
+    is_allowed: Mapped[bool] = mapped_column(Boolean)
+    created_datetime: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    updated_datetime: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
+    deleted_datetime: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
+
+class BotUserSummary(Base):
+    __tablename__ = 'telegram_bot_user_summary'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(BigInteger)
+    user_name: Mapped[str] = mapped_column(String(100))
+    last_chat_datetime: Mapped[datetime] = mapped_column(DateTime)
+    last_chat_summary: Mapped[str] = mapped_column(String(2000))
