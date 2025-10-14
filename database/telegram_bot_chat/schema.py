0db60a121e2917a35d7c7a0405e10b2c20900244
saians07 <suberlinsinaga@gmail.com>
2025-10-14 23:20:51 +0700

    Improve relation between tables

--- database/telegram_bot_chat/schema.py
+++ database/telegram_bot_chat/schema.py
@@ -4,8 +4,10 @@ from typing import (
     Optional
 )
 from sqlalchemy import (
+    ForeignKey,
     String,
     BigInteger,
+    Integer,
     Boolean,
     DateTime,
     func
@@ -20,19 +22,19 @@ class BotChatHistory(Base):
     """Model to handle Chat History"""
     __tablename__ = 'telegram_bot_chat_history'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     update_id: Mapped[int] = mapped_column(BigInteger)
     parent_id: Mapped[Optional[int]] = mapped_column(BigInteger, nullable=True)
-    user_id: Mapped[int] = mapped_column(BigInteger)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('telegram_bot_user.id'))
     created_datetime: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
     updated_datetime: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
     deleted_datetime: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
 
 class BotUserMapping(Base):
     """Model to handle User Mapping"""
-    __tablename__ = 'telegram_bot_user_mapping'
+    __tablename__ = 'telegram_bot_user'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, nprimary_key=True)
     user_id: Mapped[int] = mapped_column(BigInteger)
     user_name: Mapped[str] = mapped_column(String(100))
     is_allowed: Mapped[bool] = mapped_column(Boolean, default=True)
@@ -44,8 +46,9 @@ class BotUserSummary(Base):
     """Model to handle User Summary"""
     __tablename__ = 'telegram_bot_user_summary'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(BigInteger)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('telegram_bot_user.id'))
     user_name: Mapped[str] = mapped_column(String(100))
     last_chat_datetime: Mapped[datetime] = mapped_column(DateTime)
     last_chat_summary: Mapped[str] = mapped_column(String(2000))
+    last_chat_update_id: Mapped[int] = mapped_column(BigInteger)
