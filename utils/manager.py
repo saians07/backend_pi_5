0871d46694c48bbdaea0b3d46e85535159fc0ea4
saians07 <suberlinsinaga@gmail.com>
2025-10-17 08:33:46 +0700

    Remove db session from manager to ensure 1 time only expanded session.

--- utils/manager.py
+++ utils/manager.py
@@ -5,12 +5,6 @@
 from fastapi import FastAPI
 import httpx
 from core.telegram import TelegramBot
-from database.base import DBSession, Base, engine
-from database.telegram_bot_chat import (
-    BotChatHistory,
-    BotUserMapping,
-    BotUserSummary
-)
 
 async def lifespan(app: FastAPI):
     """
@@ -20,14 +14,5 @@ async def lifespan(app: FastAPI):
     bot = TelegramBot(client=httpx_client)
     app.state.bot = bot
 
-    dbsession = DBSession()
-    app.state.dbsession = dbsession
     yield
     await httpx_client.aclose()
-    await dbsession.close()
-
-def create_all_table():
-    """
-        Create all needed table into postgres
-    """
-    Base.metadata.create_all(engine)
