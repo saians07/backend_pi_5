ddd266fec8b0fe42064b7edd6e1029ebfefd75a6
saians07 <suberlinsinaga@gmail.com>
2025-10-18 16:26:08 +0700

    Adjust ai handler to use openai_client instead of passing client from app.

--- api/ai/gemini_handler.py
+++ api/ai/gemini_handler.py
@@ -3,6 +3,7 @@ Handlder for Gemini AI variants
 """
 import os
 from dotenv import load_dotenv
+from openai import AsyncOpenAI
 from core.logger import LOG
 from core.ai import (
     BASE_URL_GEMINI,
@@ -15,17 +16,21 @@ load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 if not GEMINI_API_KEY:
-    LOG.info("tidak ada key GEMINI_API_KEY di environment!")
+    LOG.info("Tidak ada key GEMINI_API_KEY di environment!")
+    raise RuntimeError("Tidak ada key GEMINI_API_KEY di environment.")
 
 async def ask_gemini(user_parts: dict, system_prompt: str=None) -> OpenAIStandardResponse:
     """Call gemini model to answer our inquiry. String only return."""
     LOG.info("Asking Gemini...")
+    openai_client = AsyncOpenAI(
+        base_url=BASE_URL_GEMINI, api_key=GEMINI_API_KEY
+    )
+
     resp = await base_command(
-        base_url=BASE_URL_GEMINI,
         model="gemini-2.5-flash",
-        api_key=GEMINI_API_KEY,
         system_prompt=system_prompt,
-        user_parts=user_parts
+        user_parts=user_parts,
+        client=openai_client
     )
     LOG.info("Gemini done answering.")
     return resp
--- api/ai/gpt_handler.py
+++ api/ai/gpt_handler.py
@@ -3,9 +3,9 @@ Handlder for GPT AI variants
 """
 import os
 from dotenv import load_dotenv
+from openai import AsyncOpenAI
 from core.logger import LOG
 from core.ai import (
-    BASE_PROMPT,
     BASE_URL_OPEN_ROUTER,
     base_command
 )
@@ -19,14 +19,16 @@ if not OPEN_ROUTER_API:
     LOG.info("tidak ada key GEMINI_API_KEY di environment!")
 
 async def ask_gpt_oss_20_b(user_parts: dict, system_prompt: str=None) -> OpenAIStandardResponse:
-    """Execute function to call gemini"""
+    """Execute function to call gpt oss from open router."""
     LOG.info("Asking GPT-OSS...")
+    openai_client = AsyncOpenAI(
+        base_url=BASE_URL_OPEN_ROUTER, api_key=OPEN_ROUTER_API
+    )
     resp = await base_command(
-        base_url=BASE_URL_OPEN_ROUTER,
         model="openai/gpt-oss-20b:free",
-        api_key=OPEN_ROUTER_API,
-        system_prompt=BASE_PROMPT if not system_prompt else system_prompt,
-        user_parts=user_parts
+        system_prompt=system_prompt,
+        user_parts=user_parts,
+        client=openai_client
     )
     LOG.info("GPT-OSS done answering.")
     return resp
